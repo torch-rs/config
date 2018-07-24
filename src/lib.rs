38e02ae4fb4eddfe9dfc5873e5b5ee341154e9e2
Patrick Chan <patrickchan1999@gmail.com>
2018-07-23 20:52:43 -0400

    Add Config trait - All configs should implement this trait

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+use std::io::Error;
+
+pub trait Config {
+
+    fn new(filename: String) -> Self;
+    fn get(&self, key: String) -> Option<String>;
+    fn set(&mut self, HashMap<String, String>);
+    fn save(&self) -> Result<(), Error>; 
+
+}
